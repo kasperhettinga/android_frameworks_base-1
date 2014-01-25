CyanogenMod/android_frameworks_base

SystemUI: Forward port Statusbar Clock and Date actions
Includes:

Add onClick action to notification clock and date

In expanded StatusBar
- clicking clock opens alarms
- clicking date opens calendar - today selected

http://www.youtube.com/watch?v=MLom5e3nQoA

PS3: added turn-blue-on-click
PS4++: nitpicking cleanups

Change-Id: I850732dcca0e65af59ce009aaf9f7ba18bcaf915

Add onLongClick action to notication clock and date

- As discussed with ciwrl on google+, LongClick opens DateTimeSettings
- Moved on-touch color change to superior xml solution, as suggested by
  Andrew Boren <preludedrew@gmail.com> here: http://review.evervolv.com/#/c/3676/"

Change-Id: I1fae71e347ffcb786227ba0b2651a66f2df35e27

Add SET_ALARM permission for SystemUI

If one or more third-party alarm clock app is installed, clicking clock should open
an app chooser for the alarm clock. However, permission for com.android.alarm.permission.SET_ALARM
is required to make this behaviour work properly.

See http://forum.xda-developers.com/showpost.php?p=33155463&postcount=2749 for symptom if this
permission is not assigned.

Change-Id: Iaf8cb9aba6e53da021da5373493b1419096e91fa

packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -16,13 +16,18 @@
 
 package com.android.systemui.statusbar.policy;
 
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.provider.AlarmClock;
 import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.ViewParent;
 import android.widget.TextView;
 
@@ -30,7 +35,7 @@ import com.android.systemui.R;
 
 import java.util.Date;
 
-public class DateView extends TextView {
+public class DateView extends TextView implements OnClickListener, OnLongClickListener {
     private static final String TAG = "DateView";
 
     private boolean mAttachedToWindow;
@@ -51,6 +56,8 @@ public class DateView extends TextView {
 
     public DateView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnClickListener(this);
+        setOnLongClickListener(this);
     }
 
     @Override
@@ -59,7 +66,7 @@ public class DateView extends TextView {
         mAttachedToWindow = true;
         setUpdates();
     }
-    
+
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
@@ -122,5 +129,39 @@ public class DateView extends TextView {
                 mContext.unregisterReceiver(mIntentReceiver);
             }
         }
+    }
+
+    private void collapseStartActivity(Intent what) {
+        // collapse status bar
+        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
+                Context.STATUS_BAR_SERVICE);
+        statusBarManager.collapsePanels();
+
+        // dismiss keyguard in case it was active and no passcode set
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (Exception ex) {
+            // no action needed here
+        }
+
+        // start activity
+        what.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(what);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        collapseStartActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent("android.settings.DATE_SETTINGS");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        collapseStartActivity(intent);
+
+        // consume event
+        return true;
     }
 }

kasperhettinga/android_frameworks_base-1

Port of http://review.cyanogenmod.org/#/c/27795/4
PS2: Cleanup and address comments
PS3: Remove unused variables
Begin old commit message-----------
Patch set 1-2: Initial port
Patch set 3  : Fixes date click action
Patch set 4  : Remove unused import
- Add onClick action to notification clock and date
  In expanded StatusBar
  - clicking clock opens alarms
  - clicking date opens calendar - today selected
  Id: I850732dcca0e65af59ce009aaf9f7ba18bcaf915
- Add onLongClick action to notication clock and date
  - As discussed with ciwrl on google+, LongClick opens DateTimeSettings
  - Moved on-touch color change to superior xml solution, as suggested by
    Andrew Boren <preludedrew@gmail.com> here: http://review.evervolv.com/#/c/3676/"
  Id: I1fae71e347ffcb786227ba0b2651a66f2df35e27
- Add SET_ALARM permission for SystemUI
  If one or more third-party alarm clock app is installed, clicking clock should
  open an app chooser for the alarm clock. However, permission for
  com.android.alarm.permission.SET_ALARM is required to make this behaviour
  work properly.
Change-Id: I8006e57a953fe0c4be480e0aa27a4c28c672f982

packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -16,13 +16,21 @@
 
 package com.android.systemui.statusbar.policy;
 
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.ViewParent;
 import android.widget.TextView;
 
@@ -34,7 +42,7 @@ import java.util.Locale;
 
 import libcore.icu.ICU;
 
-public class DateView extends TextView {
+public class DateView extends TextView implements OnClickListener, OnLongClickListener {
     private static final String TAG = "DateView";
 
     private final Date mCurrentTime = new Date();
@@ -63,6 +71,8 @@ public class DateView extends TextView {
 
     public DateView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnClickListener(this);
+        setOnLongClickListener(this);
     }
 
     @Override
@@ -111,5 +121,44 @@ public class DateView extends TextView {
             setText(text);
             mLastText = text;
         }
+    }
+
+    private void collapseStartActivity(Intent what) {
+        // collapse status bar
+        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
+                Context.STATUS_BAR_SERVICE);
+        statusBarManager.collapsePanels();
+
+        // dismiss keyguard in case it was active and no passcode set
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (Exception ex) {
+            // no action needed here
+        }
+
+        // start activity
+        what.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(what);
+    }
+
+    @Override
+    public void onClick(View v) {
+        long nowMillis = System.currentTimeMillis();
+
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        ContentUris.appendId(builder, nowMillis);
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+        collapseStartActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent("android.settings.DATE_SETTINGS");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        collapseStartActivity(intent);
+
+        // consume event
+        return true;
     }
 }